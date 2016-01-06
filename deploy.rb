f8b4a8451c063744f622f66119fe70318beec60c
Kevin Bacha <chewbacha@gmail.com>
2016-01-06 11:01:46 -0800

    Add vimrc to deploy

--- deploy.rb
+++ deploy.rb
@@ -2,3 +2,15 @@
 
 # TODO Prompt to copy each of the home directory files into the users home
 #      directory so that it recreates it.
+
+COPY_TO_MAPPING = {
+  '.vimrc' => '~/.vimrc'
+}
+
+root = __dir__
+Dir[File.join(root, 'home', '*')].each do |file|
+  next unless File.file?(file)
+  dest = COPY_TO_MAPPING[file]
+  next unless dest
+  File.copy(file, dest)
+end
